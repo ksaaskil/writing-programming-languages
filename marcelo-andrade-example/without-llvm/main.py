b966f87f92ed879872ed963e8bcf2ebdbc2d44d7
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-08-02 15:33:04 +0300

    Add a simpler example.

---
+++ marcelo-andrade-example/without-llvm/main.py
@@ -0,0 +1,15 @@
+
+from lexer import Lexer
+from parser import Parser
+
+text_input = """
+print(4 + 4 - 2);
+"""
+
+lexer = Lexer().get_lexer()
+tokens = lexer.lex(text_input)
+
+pg = Parser()
+pg.parse()
+parser = pg.get_parser()
+parser.parse(tokens).eval()
