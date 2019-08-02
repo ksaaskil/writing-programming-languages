ee8a5fe633b053ec848b7770b2898afb2f998bbb
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-08-02 16:45:55 +0300

    Add more logging to rply example.

--- rply/main.py
+++ rply/main.py
@@ -1,8 +1,15 @@
+import sys
 from lexer import lexer
 from parser import parser
-expression = "1 + 2 + 3 * 4"
-tokens = lexer.lex(expression)
-parsed = parser.parse(tokens)
+
+if len(sys.argv) > 2:
+    raise Exception("Got multiple input arguments")
+
+expression = sys.argv[1] if len(sys.argv) == 2 else "(1 + 2) + 3 * 4"
+
+tokens = list(lexer.lex(expression))
+print(f"Got tokens: {tokens}")
+parsed = parser.parse(iter(tokens))
 value = parsed.eval()
 print(f"Result of {expression}:")
 print(value)
--- rply/parser.py
+++ rply/parser.py
@@ -17,10 +17,12 @@ pg = ParserGenerator(
 def expression_number(p):
     # p is a list of the pieces matched by the right hand side of the
     # rule
+    print("Matched to NUMBER token:", p[0])
     return Number(int(p[0].getstr()))
 
 @pg.production('expression : OPEN_PARENS expression CLOSE_PARENS')
 def expression_parens(p):
+    print("Matched to parentheses", p)
     return p[1]
 
 @pg.production('expression : expression PLUS expression')
@@ -28,17 +30,19 @@ def expression_parens(p):
 @pg.production('expression : expression MUL expression')
 @pg.production('expression : expression DIV expression')
 def expression_binop(p):
-    left = p[0]
-    right = p[2]
-    if p[1].gettokentype() == 'PLUS':
+    print("Matched to binary operation", p)
+    left = p[0]  # `ast.Number type`
+    right = p[2]  # `ast.Number type`
+    operation_token_type = p[1].gettokentype()
+    if operation_token_type == 'PLUS':
         return Add(left, right)
-    elif p[1].gettokentype() == 'MINUS':
+    elif operation_token_type == 'MINUS':
         return Sub(left, right)
-    elif p[1].gettokentype() == 'MUL':
+    elif operation_token_type == 'MUL':
         return Mul(left, right)
-    elif p[1].gettokentype() == 'DIV':
+    elif operation_token_type == 'DIV':
         return Div(left, right)
     else:
-        raise AssertionError('Oops, this should not be possible!')
+        raise AssertionError(f'No matching operation for {operation_token_type}')
 
 parser = pg.build()
